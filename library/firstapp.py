ba0692e56ffe6c03ca8b437f317c10de560de09a
JohnFunkCode <email@johnfunk.com>
2019-04-18 21:25:12 -0600

    A few simples changes for Python 3 and Google App Engine Deployment

--- library/firstapp.py
+++ library/firstapp.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from textanalytics import textanalytics as ta
+from library.textanalytics import textanalytics as ta
 
 app = Flask(__name__)
 
